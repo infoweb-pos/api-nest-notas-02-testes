e66151cd106f2942b8a05c50c21bfbd698d23fc4
Leonardo Minora <leonardo.minora@gmail.com>
2025-09-30 21:50:25 -0300

    adicionado crud de tarefas na rota /tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'tasks.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Apenas para desenvolvimento
+    }),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
